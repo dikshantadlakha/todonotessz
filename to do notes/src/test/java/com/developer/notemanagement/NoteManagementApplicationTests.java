516350e5b0992fcbc32b51f9e4bda6b755ea88e1
dikshantadlakha <134389059+dikshantadlakha@users.noreply.github.com>
2023-07-10 16:32:25 +0530

    Add files via upload

---
+++ to do notes/src/test/java/com/developer/notemanagement/NoteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.notemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
